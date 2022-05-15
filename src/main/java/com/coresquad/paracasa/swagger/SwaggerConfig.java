d535b46a2f1881221980090bf65496468000cc54
Edwin Leal <edwinleal019@gmail.com>
2022-05-15 14:31:01 +0200

    feat: configuracion swagger añadida

---
+++ src/main/java/com/coresquad/paracasa/swagger/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.coresquad.paracasa.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(regex("/api/.*"))
+                .build()
+                .apiInfo(getApiInfo())
+                ;
+    }
+
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Para Casa API",
+                "API RestFul of ParaCasa App",
+                "1.0",
+                "Terms of service",
+                new Contact("ParaCasa", "https://www.linkedin.com/in/edwin-leal/", "paracasa@mail.com"),
+                "LICENSE",
+                "LICENSE URL",
+                Collections.emptyList()
+        );
+    }
+}
